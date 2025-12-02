8f62f1c7975f85a11967a1e1bc29eae134eb5d7b
Radosław Kaczmarczyk <radoslaw.kaczmarczyk@proton.me>
2025-12-02 12:40:19 +0100

    day 2 part 2

---
+++ day 2/part2.ts
@@ -0,0 +1,37 @@
+import { readFileSync } from 'fs';
+
+const input: string[][] = readFileSync('input.txt', 'utf-8')
+  .trim()
+  .split(',')
+  .map((range: string) => range.split('-'));
+
+const invalidNumbers: number[] = [];
+
+for (const range of input) {
+  const start: number = Number(range[0]);
+  const end: number = Number(range[1]);
+
+  for (let i = start; i <= end; i++) {
+    const cur = i.toString();
+    for (let j = 1; j <= cur.length / 2; j++) {
+      const pattern = cur.substring(0, j);
+      const parts: string[] = [];
+
+      // Break down the string to k parts
+      for (let k = 0; k < cur.length; k += pattern.length) {
+        parts.push(cur.slice(k, k + pattern.length));
+      }
+
+      const allEqual: boolean = parts.every(v => v === parts[0]);
+
+      if (allEqual) {
+        invalidNumbers.push(i);
+        break;
+      }
+    }
+  }
+}
+
+const sum = invalidNumbers.reduce((sum, num) => (sum += num));
+
+console.log(sum);
