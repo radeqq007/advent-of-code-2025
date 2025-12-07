bae7e90b429c473ed2ae85fd2c7548499e20a15c
radeqq007 <radeqq007@interia.pl>
2025-12-07 12:13:27 +0100

    day 7 part 1

---
+++ day 7/part1.ts
@@ -0,0 +1,23 @@
+import { readInput } from '../utils/index.ts';
+
+const input: string[][] = readInput('day 7/input.txt')
+  .split('\r\n')
+  .map((line: string) => line.split(''));
+
+const beamStartCol = input[0].indexOf('S');
+let beams: number[] = [beamStartCol];
+
+let count: number = 0;
+
+for (let row = 1; row < input.length; row++) {
+  for (const beamCol of beams) {
+    if (input[row][beamCol] === '^') {
+      beams = beams.filter(val => val != beamCol);
+      if (!beams.includes(beamCol - 1)) beams.push(beamCol - 1);
+      if (!beams.includes(beamCol + 1)) beams.push(beamCol + 1);
+      count++;
+    }
+  }
+}
+
+console.log(count);
