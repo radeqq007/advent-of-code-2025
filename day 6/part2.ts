e0f91b9c6b470818ece3cd16eeb59fa8e356c699
radeqq007 <radeqq007@interia.pl>
2025-12-07 11:46:37 +0100

    oh my god bruh

--- day 6/part2.ts
+++ day 6/part2.ts
@@ -1,7 +1,7 @@
 import { readInput } from '../utils/index.ts';
 
 const input = readInput('day 6/input.txt')
-  .split('\r\n')
+  .split('\n')
   .map((line: string) => line.split(''));
 
 const operations: string[] = input.at(-1).filter((op: string) => op != ' ');
