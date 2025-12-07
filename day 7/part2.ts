a2e9cf79df1cdefb424de150980374cd88292a48
radeqq007 <radeqq007@interia.pl>
2025-12-07 13:18:38 +0100

    day 7 part 2

---
+++ day 7/part2.ts
@@ -0,0 +1,39 @@
+import { readInput } from '../utils/index.ts';
+
+const input: string[][] = readInput('day 7/input.txt')
+  .split('\n')
+  .map((line: string) => line.split(''));
+
+const beamStartCol = input[0].indexOf('S');
+
+const memo = new Map<string, number>(); // "row,col"
+
+type timeline = {
+  leftTimeline?: timeline;
+  rightTimeline?: timeline;
+};
+
+const generateTimeline = (row: number, col: number): number => {
+  const key = `${row},${col}`;
+  if (memo.has(key)) return memo.get(key)!;
+
+  if (row + 1 >= input.length) {
+    return 1;
+  }
+
+  let ways = 0;
+
+  if (input[row][col] === '^') {
+    if (col > 0) ways += generateTimeline(row + 1, col - 1);
+    if (col < input[0].length - 1) ways += generateTimeline(row + 1, col + 1);
+  } else {
+    ways += generateTimeline(row + 1, col);
+  }
+
+  memo.set(key, ways);
+  return ways;
+};
+
+let count = generateTimeline(1, beamStartCol);
+
+console.log(count);
